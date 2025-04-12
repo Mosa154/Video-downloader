5211d12062d8274eb887d88b9926ee060230f5f4
mosa154 <mosa1616sa@gmail.com>
2025-04-12 09:14:28 +0300

    تعديل

--- app.py
+++ app.py
@@ -34,19 +34,8 @@ def download_video():
             ydl.download([video_url])
 
         return render_template('index.html', download_url=f'/get_video/{video_id}')
+
     except Exception as e:
-    print("خطأ أثناء تحميل الفيديو:", e)  # هذا راح يطبع الخطأ في لوق Render
-    return render_template('index.html', error="فشل في تحميل الفيديو. تأكد من صحة الرابط.")
+        print("خطأ أثناء تحميل الفيديو:", e)
+        return render_template('index.html', error="فشل في تحميل الفيديو. تأكد من صحة الرابط.")
 
-
-
-@app.route('/get_video/<video_id>')
-def get_video(video_id):
-    filepath = os.path.join(DOWNLOAD_DIR, f"{video_id}.mp4")
-    if os.path.exists(filepath):
-        return send_file(filepath, as_attachment=True)
-    return "الملف غير موجود", 404
-
-if __name__ == '__main__':
-
-    app.run(host='0.0.0.0', port=5000)
